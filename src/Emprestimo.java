d3a443aeae2d722c95e3543ed4c32657dc63e8fa
Gustavo Bomfim <n4megustavo@gmail.com>
2026-04-08 13:14:10 -0300

    Primeiro commit

---
+++ src/Emprestimo.java
@@ -0,0 +1,57 @@
+import java.util.Date;
+
+public class Emprestimo {
+    private int id;
+    private Livro livro;
+    private String nomeCliente;
+    private Date dataEmprestimo;
+    private Date dataDevolucao;
+
+    public Emprestimo(int id, Livro livro, String nomeCliente, Date dataEmprestimo, Date dataDevolucao) {
+        this.id = id;
+        this.livro = livro;
+        this.nomeCliente = nomeCliente;
+        this.dataEmprestimo = dataEmprestimo;
+        this.dataDevolucao = dataDevolucao;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Livro getLivro() {
+        return livro;
+    }
+
+    public void setLivro(Livro livro) {
+        this.livro = livro;
+    }
+
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+
+    public void setNomeCliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+
+    public Date getDataEmprestimo() {
+        return dataEmprestimo;
+    }
+
+    public void setDataEmprestimo(Date dataEmprestimo) {
+        this.dataEmprestimo = dataEmprestimo;
+    }
+
+    public Date getDataDevolucao() {
+        return dataDevolucao;
+    }
+
+    public void setDataDevolucao(Date dataDevolucao) {
+        this.dataDevolucao = dataDevolucao;
+    }
+}
---
+++ src/Livro.java
@@ -0,0 +1,67 @@
+import java.util.Date;
+
+public class Livro {
+    private int id;
+    private String titulo;
+    private Autor autor;
+    private boolean disponivel;
+    private Date dataCadastro;
+    private Date dataAtualizacao;
+
+    public Livro(int id, String titulo, Autor autor, boolean disponivel, Date dataCadastro, Date dataAtualizacao) {
+        this.id = id;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.disponivel = disponivel;
+        this.dataCadastro = dataCadastro;
+        this.dataAtualizacao = dataAtualizacao;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Autor getAutor() {
+        return autor;
+    }
+
+    public void setAutor(Autor autor) {
+        this.autor = autor;
+    }
+
+    public boolean isDisponivel() {
+        return disponivel;
+    }
+
+    public void setDisponivel(boolean disponivel) {
+        this.disponivel = disponivel;
+    }
+
+    public Date getDataCadastro() {
+        return dataCadastro;
+    }
+
+    public void setDataCadastro(Date dataCadastro) {
+        this.dataCadastro = dataCadastro;
+    }
+
+    public Date getDataAtualizacao() {
+        return dataAtualizacao;
+    }
+
+    public void setDataAtualizacao(Date dataAtualizacao) {
+        this.dataAtualizacao = dataAtualizacao;
+    }
+}
