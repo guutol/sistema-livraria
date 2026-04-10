8ab72ade17553891f022488d62ce7e153b76a32b
Gustavo Bomfim <n4megustavo@gmail.com>
2026-04-09 22:50:23 -0300

    Adicionar emprestimo e encerrar sistema

--- src/Biblioteca.java
+++ src/Biblioteca.java
@@ -12,14 +12,52 @@ public class Biblioteca {
         autores.add(new Autor(2, "William Shakespeare", LocalDate.of(1564, 4, 23)));
         autores.add(new Autor(3, "Franz Kafka", LocalDate.of(1883, 7, 3)));
 
-        livros.add(new Livro(1, "Meditações",    autores.get(0), false, LocalDate.of(180, 1, 1),  LocalDate.of(180, 12, 31)));
+        livros.add(new Livro(1, "Meditações",    autores.get(0), true, LocalDate.of(180, 1, 1),  LocalDate.of(180, 12, 31)));
         livros.add(new Livro(2, "Hamlet",        autores.get(1), true, LocalDate.of(1603, 1, 1), LocalDate.of(1603, 12, 31)));
         livros.add(new Livro(3, "A Metamorfose", autores.get(2), true, LocalDate.of(1915, 1, 1), LocalDate.of(1915, 12, 31)));
     }
 
     public void listarDisponiveis() {
+        System.out.println(" ");
         for(Livro l : livros)
             if(l.isDisponivel())
                 System.out.println("ID: " + l.getId() + " Nome: " + l.getTitulo() + " está disponível.");
+    }
+
+    public void realizarEmprestimo(String livro) {
+        System.out.println(" ");
+        boolean isNumeric = livro.matches("[0-9]+"), achou = false;
+        if(isNumeric) {
+            int id = Integer.parseInt(livro);
+            for(Livro l : livros) {
+                if(l.getId() == id) {
+                    if(l.isDisponivel()) {
+                        System.out.println("ID: " + l.getId() + " Nome: " + l.getTitulo() + ". Empréstimo realizado com sucesso, boa leitura!");
+                        l.setDisponivel(false);
+                    }
+                    else {
+                        System.out.println("O livro não está disponível.");
+                    }
+                    achou = true;
+                }
+            }
+        }
+        else {
+            for(Livro l : livros) {
+                if(l.getTitulo().equalsIgnoreCase(livro)) {
+                    if(l.isDisponivel()) {
+                        System.out.println("ID: " + l.getId() + " Nome: " + l.getTitulo() + ". Empréstimo realizado com sucesso, boa leitura!");
+                        l.setDisponivel(false);
+                    }
+                    else {
+                        System.out.println("O livro não está disponível.");
+                    }
+                    achou = true;
+                }
+            }
+        }
+        if(!achou) {
+            System.out.println("Livro não encontrado na biblioteca.");
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,32 @@
 void main() {
     Scanner scanner = new Scanner(System.in);
     Biblioteca biblioteca = new Biblioteca();
+    boolean encerrar = false;
 
     try {
         System.out.println("Deseja ver a lista de livros disponíveis?");
         String resposta = scanner.nextLine();
-        while (resposta.equalsIgnoreCase("sim")) {
+        while (resposta.equalsIgnoreCase("sim") && !encerrar) {
             biblioteca.listarDisponiveis();
             System.out.println(" ");
-            System.out.println("Deseja ver a lista de livros disponíveis novamente?");
+            System.out.println("Deseja realizar empréstimo de algum livro?");
             resposta = scanner.nextLine();
+            if(resposta.equalsIgnoreCase("sim")) {
+                System.out.println(" ");
+                System.out.println("Digite o ID ou nome do livro para realizar o empréstimo:");
+                resposta = scanner.nextLine();
+                biblioteca.realizarEmprestimo(resposta);
+            }
+            else {
+                System.out.println(" ");
+                System.out.println("Ok, tenha um bom dia.");
+                encerrar = true;
+            }
+            if(!encerrar) {
+                System.out.println(" ");
+                System.out.println("Deseja ver a lista de livros disponíveis novamente?");
+                resposta = scanner.nextLine();
+            }
         }
     } catch (Exception e) {
         System.out.println("Ocorreu um erro: " + e.getMessage());;
